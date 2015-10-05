7024ecf3388955ffc8293526e0ae9cbffe16e8eb
Nikolai Vazquez <nvazquez1297@gmail.com>
2015-10-05 15:47:51 -0400

    Create Xcode project

---
+++ CaveExplorer/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CaveExplorer
+//
+//  Created by Nikolai Vazquez on 10/5/15.
+//  Copyright © 2015 CCHS Coding Club. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
