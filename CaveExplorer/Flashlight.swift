853455e2fb4b698043845e05e8a43df4853f00ac
Nikolai Vazquez <nvazquez1297@gmail.com>
2015-10-14 15:40:11 -0400

    Create Flashlight class

---
+++ CaveExplorer/Flashlight.swift
@@ -0,0 +1,13 @@
+//
+//  Flashlight.swift
+//  CaveExplorer
+//
+//  Created by Nikolai Vazquez on 10/14/15.
+//  Copyright © 2015 CCHS Coding Club. All rights reserved.
+//
+
+import Foundation
+
+final class Flashlight: Item {
+    var batteryLife: Double = 100
+}
