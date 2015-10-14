39a8a7014e0e7b1197383ceea3d30d756583f444
Nikolai Vazquez <nvazquez1297@gmail.com>
2015-10-14 15:50:28 -0400

    Add on property to Flashlight

--- CaveExplorer/Flashlight.swift
+++ CaveExplorer/Flashlight.swift
@@ -9,5 +9,27 @@
 import Foundation
 
 final class Flashlight: Item {
+
     var batteryLife: Double = 100
+
+    var on: Bool = false
+
+}
+
+extension Player {
+
+    var flashlight: Flashlight {
+        get {
+            guard let flashlight = items[Flashlight.Identifier] as? Flashlight else {
+                let flashlight = Flashlight()
+                self.items[Flashlight.Identifier] = flashlight
+                return flashlight
+            }
+            return flashlight
+        }
+        set {
+            self.items[Flashlight.Identifier] = newValue
+        }
+    }
+
 }
